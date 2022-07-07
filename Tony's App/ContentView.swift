954b1aa66bd7262901c3835313ac2d415839f922
Anthony Ward <pants1984@gmail.com>
2022-07-07 12:45:38 +0100

    Initial Commit

---
+++ Tony's App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Tony's App
+//
+//  Created by Anthony Ward on 07/07/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Tony's App/Tony_s_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tony_s_AppApp.swift
+//  Tony's App
+//
+//  Created by Anthony Ward on 07/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct Tony_s_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
